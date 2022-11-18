a6f068dafef580a82c5ca205030460e0fb68a9dc
rjwehrle <rjwehrle@gmail.com>
2022-11-17 21:08:54 -0500

    Initial services

---
+++ MetadataService.cs
@@ -0,0 +1,74 @@
+﻿using Famecipe.Common;
+using Famecipe.Models;
+
+namespace Famecipe.Services;
+
+public class MetadataService
+{
+    public readonly IRepository<Metadata> _repository;
+
+    public MetadataService(IRepository<Metadata> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Metadata> CreateMetadata(Metadata metadata)
+    {
+        try
+        {
+            return await _repository.Create(metadata);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task DeleteMetadata(string key)
+    {
+        try
+        {
+            await _repository.Delete(key);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task UpdateMetadata(string key, Metadata metadata)
+    {
+        try
+        {
+            await _repository.Update(key, metadata);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<IEnumerable<Metadata>> GetMetadata()
+    {
+        try
+        {
+            return await _repository.Get();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<Metadata> GetMetadata(string key)
+    {
+        try
+        {
+            return await _repository.Get(key);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
---
+++ RecipeService.cs
@@ -0,0 +1,77 @@
+﻿using Famecipe.Common;
+using Famecipe.Models;
+
+namespace Famecipe.Services;
+
+public class RecipeService
+{
+    public readonly IRepository<Recipe> _repository;
+
+    public RecipeService(IRepository<Recipe> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Recipe> CreateRecipe(Recipe Recipe)
+    {
+        try
+        {
+            Recipe.WhenUpdatedUTC = DateTime.UtcNow;
+            Recipe.Identifier = Guid.NewGuid().ToString();
+            return await _repository.Create(Recipe);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task DeleteRecipe(string identifier)
+    {
+        try
+        {
+            await _repository.Delete(identifier);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task UpdateRecipe(string identifier, Recipe recipe)
+    {
+        try
+        {
+            recipe.WhenUpdatedUTC = DateTime.UtcNow;
+            await _repository.Update(identifier, recipe);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<IEnumerable<Recipe>> GetRecipes()
+    {
+        try
+        {
+            return await _repository.Get();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<Recipe> GetRecipe(string identifier)
+    {
+        try
+        {
+            return await _repository.Get(identifier);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
